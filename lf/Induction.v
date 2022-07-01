f9888944fae7f0e659ff45fbc9b0e338a4e2ae00
wrongbyte <57643375+wrongbyte@users.noreply.github.com>
2022-07-01 18:33:17 -0300

    proof by induction

--- lf/Induction.v
+++ lf/Induction.v
@@ -44,7 +44,7 @@ Proof.
   - reflexivity.
   - rewrite <- IHn'. simpl. reflexivity. Qed.
 
-(** Exercise 1, 2 stars **)
+(** Exercise 1 - basic induction, 2 stars **)
 Theorem mul_0_r: forall n: nat, n * 0 = 0.
 Proof.
   intros n. induction n as [| n' IHn'].
@@ -75,4 +75,71 @@ Proof.
     reflexivity.
   Qed.
     
+Theorem add_assoc : forall n m p: nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p.
+  induction n as [| n' IHn'].
+  - simpl. reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+  Qed.
 
+(** Exercise 2 - double plus, 2 stars**)
+
+Fixpoint double (n: nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+
+(** Use induction to prove this fact **)
+Lemma double_plus: forall n, double n = n + n.
+Proof.
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - simpl. rewrite IHn'. rewrite plus_n_Sm. reflexivity.
+  Qed.
+
+
+(**
+Fixpoint even (n:nat) : bool :=
+  match n with
+  | O        => true
+  | S O      => false
+  | S (S n') => even n'
+  end.
+**)
+
+(** Optional exercise - using induction to prove a lemma about even function **)
+Theorem even_S : forall n : nat,
+  even (S n) = negb (even n).
+Proof.
+  induction n as [| n' IHn'].
+  - reflexivity.
+  - rewrite IHn'. 
+    rewrite negb_involutive. 
+    simpl. reflexivity.
+Qed.
+
+(** Differences destruct and induction **)
+
+(**
+  
+  TL;DR Their main difference is the subgoal hypothesis.  
+  
+  The destruct tactic gives us a subgoal for each constructor of 
+  an inductive type. It means that we have to prove that a proposition
+  holds for both O or S n' for a nat type, for example.
+  This creates as many goals as there are constructors in type T.
+  
+  On the other hand, induction generates subgoals as many as 
+  there are constructors, and adds the inductive hypotheses in 
+  the contexts. 
+  
+**)
+
+(* ################################################################# *)
+(** * Proofs Within Proofs *)
+
+    
